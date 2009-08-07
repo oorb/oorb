a8b2d86c876bce4b033f3ca6f558373b8757423d
fpierfed <fpierfed@8b1897c2-6a67-11de-89a9-0b20f3e2dceb>
2009-08-07 22:32:41 +0000

    updated/added GPL notice

--- trunk/python/utils.py
+++ trunk/python/utils.py
@@ -1,20 +1,25 @@
-# This file is part of OpenOrb-Python.
-# 
-# OpenOrb-Python is free software: you can redistribute it and/or modify
+#
+# LSST Data Management System
+# Copyright 2008, 2009 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
-# OpenOrb-Python is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with OpenOrb-Python.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# Author: F. Pierfederici <fpierfed@gmail.com>
-# 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+# F. Pierfederici <fpierfed@gmail.com>
 from constants import *
 from api import *
 
