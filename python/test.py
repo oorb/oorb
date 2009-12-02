63957a1f27c7c6836bcdcb949494147133bc8c59
mgranvik <mgranvik@iki.fi>
2009-12-02 02:36:50 +0000

    Sliced oorb_ephemeris into two [oorb_ephemeris (no uncertainties) and oorb_ephemeris_covariance] to get rid of a persistent bug where python would not recognize optional arguments for oorb_ephemeris.

--- python/test.py
+++ python/test.py
@@ -41,8 +41,7 @@ if __name__ == "__main__":
     #keplerian appears to be element type index 3
     #orbits = numpy.array([0.,1.,2.,3.,4.,5.,6.,5373.,1.,1.,3.])
     #using the first item in PS-SSM, 1/100th density, s1.01 file.
-    orbits = numpy.zeros([2,12], dtype=numpy.double, order='F')
-    print orbits
+    orbits = numpy.zeros([3,12], dtype=numpy.double, order='F')
     orbits[0][0] = 1.0
     orbits[0][1] = 1.853233422926951E+00
     orbits[0][2] = 3.871022198610788E-01
@@ -55,16 +54,19 @@ if __name__ == "__main__":
     orbits[0][7] = 3.0
     orbits[0][8] = 55200.0
     orbits[0][9] = 3.0
-    orbits[0][10]= 24.541
+    orbits[0][10]= 21.541
     orbits[0][11]= .15
 
     orbits[1][0] = 2.0
+    orbits[2][0] = 3.0
     for i in range(1,12):
         orbits[1][i] = orbits[0][i]
-    
+        orbits[2][i] = orbits[0][i]
     print orbits[0]
     print orbits[1]
-    covariances = numpy.zeros([2,6,6], order='F') #zero covariance is okay for now...
+    print orbits[2]
+
+    covariances = numpy.zeros([3,6,6], order='F') #zero covariance is okay for now...
 
     covariances[0][0][:] = [ 1.59357530E-06, 4.54646999E-07, 5.59222797E-06, \
                               -3.87304158E-06, -3.54135866E-07, -4.31574921E-05 ]
@@ -87,6 +89,7 @@ if __name__ == "__main__":
     for i in range(0,6):
         for j in range(0,6):
             covariances[1][i][j] = covariances[0][i][j]
+            covariances[2][i][j] = covariances[0][i][j]
     
     
     obscode = "500"
@@ -94,23 +97,21 @@ if __name__ == "__main__":
     ephem_dates[0][:] = [ 55148.0, 1.0 ]
     ephem_dates[1][:] = [ 55150.0, 1.0 ]
 
-    print "calling oorb_ephemeris with covariances"
-    eph, err = pyoorb.pyoorb.oorb_ephemeris(in_orbits=orbits, \
-                                                in_covariances=covariances, \
-                                                in_obscode=obscode, \
-                                                in_date_ephems=ephem_dates)
+    print "calling oorb_ephemeris_covariance"
+    eph, err = pyoorb.pyoorb.oorb_ephemeris_covariance(orbits, \
+                                                covariances, \
+                                                obscode, \
+                                                ephem_dates)
     print "error code:", err
-    print eph[0][0][:]
-    print eph[0][1][:]
-    print eph[1][0][:]
-    print eph[1][1][:]
+    for i in range(0,3):
+        for j in range(0,2):
+            print eph[i][j]
 
-    print "calling oorb_ephemeris without covariances"
+    print "calling oorb_ephemeris"
     eph, err = pyoorb.pyoorb.oorb_ephemeris(in_orbits=orbits, \
                                                 in_obscode=obscode, \
                                                 in_date_ephems=ephem_dates)
     print "error code:", err
-    print eph[0][0][:]
-    print eph[0][1][:]
-    print eph[1][0][:]
-    print eph[1][1][:]
+    for i in range(0,3):
+        for j in range(0,2):
+            print eph[i][j]
