a28919f7356b5c0e4f5a1b8f86de0c6e4b52fef2
mgranvik <mgranvik@iki.fi>
2010-04-07 09:34:21 +0000

    Added possibility to input calendar dates.

--- main/tico.f90
+++ main/tico.f90
@@ -1,6 +1,6 @@
 !====================================================================!
 !                                                                    !
-! Copyright 2002,2003,2004,2005,2006,2007,2008,2009                  !
+! Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010             !
 ! Mikael Granvik, Jenni Virtanen, Karri Muinonen, Teemu Laakso,      !
 ! Dagmara Oszkiewicz                                                 !
 !                                                                    !
@@ -43,7 +43,7 @@
 !!
 !! AUTHOR
 !!
-!! Mikael Granvik 2009-06-18
+!! Mikael Granvik 2010-03-31
 !!
 PROGRAM tico
 
@@ -53,6 +53,7 @@ PROGRAM tico
 
   IMPLICIT NONE
   TYPE (Time) :: t
+  CHARACTER(len=64) :: str
   REAL(bp) :: mjd_in, mjd_out
 
   ! Set path to data files:
@@ -60,24 +61,24 @@ PROGRAM tico
 
   ! Input
   IF (get_cl_option("--tai_in=", .FALSE.)) THEN
-     mjd_in = get_cl_option("--tai_in=", 0.0_bp)
-     CALL NEW(t, mjd_in, "TAI")
+     str = get_cl_option("--tai_in=", "")
+     CALL readDate(str, "TAI", t)
      IF (error) THEN
         CALL errorMessage("tico", &
              "TRACE BACK (5)", 1)
         STOP
      END IF
   ELSE IF (get_cl_option("--utc_in=", .FALSE.)) THEN
-     mjd_in = get_cl_option("--utc_in=", mjd_in)
-     CALL NEW(t, mjd_in, "UTC")
+     str = get_cl_option("--utc_in=", "")
+     CALL readDate(str, "UTC", t)
      IF (error) THEN
         CALL errorMessage("tico", &
              "TRACE BACK (10)", 1)
         STOP
      END IF
   ELSE IF (get_cl_option("--tt_in=", .FALSE.)) THEN
-     mjd_in = get_cl_option("--tt_in=", mjd_in)
-     CALL NEW(t, mjd_in, "TT")
+     str = get_cl_option("--tt_in=", "")
+     CALL readDate(str, "TT", t)
      IF (error) THEN
         CALL errorMessage("tico", &
              "TRACE BACK (15)", 1)
@@ -119,5 +120,46 @@ PROGRAM tico
           "Output timescale not specified.", 1)
   END IF
   CALL NULLIFY(t)
+
+CONTAINS
+
+  SUBROUTINE readDate(str, timescale, t)
+
+    IMPLICIT NONE
+    TYPE (Time), INTENT(out) :: t
+    CHARACTER(len=*), INTENT(in) :: str, timescale
+    REAL(bp) :: mjd_in, day
+    INTEGER, DIMENSION(0:2) :: indx_arr
+    INTEGER :: i, j, year, month
+
+    ! Count hyphens to figure out if this is a calendar date or MJD:
+    indx_arr = 0
+    i = 0
+    j = 1
+    DO WHILE (j > 0)
+       j = INDEX(str(indx_arr(i)+1:),"-")
+       IF (j > 0) THEN
+          indx_arr(i+1) = indx_arr(i) + j
+          i = i + 1
+       END IF
+    END DO
+    IF (i > 1) THEN
+       CALL toInt(str(1:indx_arr(1)-1), year, error)
+       CALL toInt(str(indx_arr(1)+1:indx_arr(2)-1), month, error)
+       CALL toReal(TRIM(str(indx_arr(2)+2:)), day, error)
+       !write(*,*) year, month, day
+       CALL NEW(t, year, month, day, TRIM(timescale))
+    ELSE
+       CALL toReal(str, mjd_in, error)
+       CALL NEW(t, mjd_in, TRIM(timescale))
+    END IF
+    IF (error) THEN
+       CALL errorMessage("tico / readDate", &
+            "TRACE BACK", 1)
+       RETURN
+    END IF
+
+  END SUBROUTINE readDate
+
 
 END PROGRAM tico
