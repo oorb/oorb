831c30f8fb7b1662d5aa30852cc906372532ab23
Spencer Nelson <spencer@b612foundation.org>
2023-02-10 10:21:24 -0800

    Use Python to look up extension suffix, not python3-config which is nonstandard

--- build-tools/get-python-extension-suffix.sh
+++ build-tools/get-python-extension-suffix.sh
@@ -8,5 +8,5 @@ PYVER=$($PYTHON -c 'import sys; print(sys.version_info[0])')
 if [[ $PYVER == 2 ]]; then
 	echo ".so"
 elif [[ $PYVER == 3 ]]; then
-	python3-config --extension-suffix
+	$PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))'
 fi
